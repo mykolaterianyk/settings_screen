0dbdd6821dee70f87dab64fe197a8359f964a348
Mykola <mykola.terianyk@drzewniak.slupsk.pl>
2023-02-10 12:26:08 +0100

    UI design

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:settings_screen/widgets/settings_tile.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -17,8 +18,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Column(
-          children: [
-           const Text(
+          children: const [
+           Text(
               "Settings",
               style: TextStyle(
               fontFamily: 'Spoof',
@@ -26,39 +27,63 @@ class _SettingsScreenState extends State<SettingsScreen> {
               fontWeight: FontWeight.bold
             ),
             ),
-            const SizedBox(height: 40,),
-            Row(
-              children: [
-                Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.black,
-                  ),
-                  child: const Icon(
-                    Ionicons.moon_outline,
-                    color: Colors.white,),
-                ),
-                const SizedBox(width: 10),
-                Text("Dark mode",
-                style: TextStyle(
+            SizedBox(height: 40,),
+            SettingsTile(
+                color: Colors.black,
+                icon: Ionicons.moon,
+                title: 'Dark mode',
+                iconColor: Colors.white,
+            ),
+            SizedBox(height: 10,),
+            Text("Profile",
+              style: TextStyle(
+              fontFamily: 'Spoof',
+                fontSize: 18,
+                color: Color.fromARGB(255, 128, 128, 128)
+            ),),
+            SizedBox(height: 10,),
+            SettingsTile(
+                color: Color.fromARGB(100, 12, 167, 137),
+                icon: Ionicons.pencil,
+                title: 'Edit Profile',
+                iconColor: Colors.black,
+            ),
+            SizedBox(height: 10,),
+            SettingsTile(
+                color: Color.fromARGB(100, 45, 155, 240),
+                icon: Ionicons.lock_closed,
+                title: 'Change Password',
+                iconColor: Colors.black,
+            ),
+            SizedBox(height: 10,),
+            Text("Notifications",
+              style: TextStyle(
                   fontFamily: 'Spoof',
                   fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-                const SizedBox(width: 10),
-                Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(1, 12, 167, 137),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                )
-              ],
+                  color: Color.fromARGB(255, 128, 128, 128)
+              ),),
+            SizedBox(height: 10,),
+            SettingsTile(
+                color: Color.fromARGB(100, 95, 99, 251),
+                icon: Ionicons.notifications,
+                title: 'Notifications',
+                iconColor: Colors.black ,
             ),
+            SizedBox(height: 10,),
+            Text("Regional",
+              style: TextStyle(
+                  fontFamily: 'Spoof',
+                  fontSize: 18,
+                  color: Color.fromARGB(255, 128, 128, 128)
+              ),),
+            SizedBox(height: 10,),
+            SettingsTile(
+                color: Color.fromARGB(100, 250, 199, 16),
+                icon: Ionicons.language,
+                title: 'Language',
+                iconColor: Colors.black,
+            ),
+            SizedBox(height: 10,),
           ],
         ),
       ),
