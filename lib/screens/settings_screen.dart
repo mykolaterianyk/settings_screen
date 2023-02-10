4ac6833f45772fd2059a734ae42e2428bbd13bd0
Mykola <mykola.terianyk@drzewniak.slupsk.pl>
2023-02-10 12:31:39 +0100

    UI design

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -16,74 +16,74 @@ class _SettingsScreenState extends State<SettingsScreen> {
     return Scaffold(
     body: SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
            Text(
               "Settings",
               style: TextStyle(
               fontFamily: 'Spoof',
-              fontSize: 30,
+              fontSize: 40,
               fontWeight: FontWeight.bold
             ),
             ),
-            SizedBox(height: 40,),
+            SizedBox(height: 20,),
             SettingsTile(
                 color: Colors.black,
                 icon: Ionicons.moon,
                 title: 'Dark mode',
                 iconColor: Colors.white,
             ),
-            SizedBox(height: 10,),
+            SizedBox(height: 20,),
             Text("Profile",
               style: TextStyle(
               fontFamily: 'Spoof',
-                fontSize: 18,
+                fontSize: 25,
                 color: Color.fromARGB(255, 128, 128, 128)
             ),),
-            SizedBox(height: 10,),
+            SizedBox(height: 20,),
             SettingsTile(
                 color: Color.fromARGB(100, 12, 167, 137),
                 icon: Ionicons.pencil,
                 title: 'Edit Profile',
                 iconColor: Colors.black,
             ),
-            SizedBox(height: 10,),
+            SizedBox(height: 20,),
             SettingsTile(
                 color: Color.fromARGB(100, 45, 155, 240),
                 icon: Ionicons.lock_closed,
                 title: 'Change Password',
                 iconColor: Colors.black,
             ),
-            SizedBox(height: 10,),
+            SizedBox(height: 20,),
             Text("Notifications",
               style: TextStyle(
                   fontFamily: 'Spoof',
-                  fontSize: 18,
+                  fontSize: 25,
                   color: Color.fromARGB(255, 128, 128, 128)
               ),),
-            SizedBox(height: 10,),
+            SizedBox(height: 20,),
             SettingsTile(
                 color: Color.fromARGB(100, 95, 99, 251),
                 icon: Ionicons.notifications,
                 title: 'Notifications',
                 iconColor: Colors.black ,
             ),
-            SizedBox(height: 10,),
+            SizedBox(height: 20,),
             Text("Regional",
               style: TextStyle(
                   fontFamily: 'Spoof',
-                  fontSize: 18,
+                  fontSize: 25,
                   color: Color.fromARGB(255, 128, 128, 128)
               ),),
-            SizedBox(height: 10,),
+            SizedBox(height: 20,),
             SettingsTile(
                 color: Color.fromARGB(100, 250, 199, 16),
                 icon: Ionicons.language,
                 title: 'Language',
                 iconColor: Colors.black,
             ),
-            SizedBox(height: 10,),
           ],
         ),
       ),
